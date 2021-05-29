daa1138a2df1973290b920e75787b34868ee4efa
Yungang <yunganw@amazon.com>
2021-05-29 21:22:55 +1000

    force change password
    add force change password status handle

--- src/views/Hogwarts.vue
+++ src/views/Hogwarts.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="container">
-         <div class="modal-dialog">
-            <div class="modal-content background-customizable modal-content-desktop">
-                <div>
-                    <div class="banner-customizable">
-                        <center>
-                            
-                        </center>
-                    </div>
-                </div>
-                <div class="modal-body">
-                    <div>
-                        <center>
-                            <b-spinner variant="primary" label="Spinning" /><p></p>
-                            <span > Loading
-                            <b-icon icon="three-dots" animation="cylon"></b-icon>
-                            </span>
-                        </center>
-                    </div>
-                </div>
-            </div>
+  <div class="container">
+    <div class="modal-dialog">
+      <div class="modal-content background-customizable modal-content-desktop">
+        <div>
+          <div class="banner-customizable">
+            <center></center>
+          </div>
         </div>
+        <div class="modal-body">
+          <div>
+            <center>
+              <b-spinner variant="primary" label="Spinning" />
+              <p></p>
+              <span>
+                Loading
+                <b-icon icon="three-dots" animation="cylon"></b-icon>
+              </span>
+            </center>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -31,11 +31,6 @@ const MAGICRESPONSEURL = awsconfig.magicresponse_url;
 
 export default {
     name: 'Hogwarts',
-    data() {
-        return {
-            info: '',
-        };
-    },
     mounted: function() {
       axios 
         .post(MAGICRESPONSEURL,{
@@ -50,33 +45,38 @@ export default {
                     magicstring: this.$route.query.answer,
                 })
                 .then(response => {
-                    let obj = JSON.parse(response.data.body);
+                  console.log ("hgwd:", response);
+                    if (200 != response.data.statusCode) {
+                        alert (response.data.body);
+                        this.$router.push("/");
+                    }
+                    else {
+                        let obj = JSON.parse(response.data.body);
 
-                    const tokens = obj.IdToken.split('.');
-                    const tokenObj = JSON.parse(Buffer.from(tokens[1], 'base64').toString());
-                    const currentDate = new Date(tokenObj["exp"]*1000);
-                    
-                    this.$router.push({
-                        name: "UserInfo",
-                        params: {
-                            username: tokenObj["cognito:username"],
-                            role: tokenObj["cognito:roles"],
-                            group: tokenObj["cognito:groups"],
-                            email: tokenObj["email"],
-                            exp: currentDate.toLocaleString(),
-                            timezone: currentDate.toString().match(/\((.*)\)/).pop(),
-                        }
-                    });
-                
-
+                        const tokens = obj.AuthenticationResult.IdToken.split('.');
+                        const tokenObj = JSON.parse(Buffer.from(tokens[1], 'base64').toString());
+                        const currentDate = new Date(tokenObj["exp"]*1000);
+                        
+                        this.$router.push({
+                            name: "UserInfo",
+                            params: {
+                                username: tokenObj["cognito:username"],
+                                role: tokenObj["cognito:roles"],
+                                group: tokenObj["cognito:groups"],
+                                email: tokenObj["email"],
+                                exp: currentDate.toLocaleString(),
+                                timezone: currentDate.toString().match(/\((.*)\)/).pop(),
+                            }
+                        });
+                    }
                 })
                 .catch(function (error) { 
-                    this.info = error;
+                    alert(error.message);
                     console.log(error);
             });          
         })
         .catch(function (error) { 
-            this.info = error
+            alert(error.message);
             console.log(error);
         });
     },
